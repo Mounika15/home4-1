e716697ade2220a98d4f49a0f47f184f22ac17ce
balakumarhs <bs443@njit.edu>
2017-02-23 23:25:27 -0500

    adding rest of the code in add_category.php

--- add_category.php
+++ add_category.php
@@ -1,3 +1,14 @@
 <?php
 $categoryName = filter_input(INPUT_POST, 'categoryName');
+
+require_once('database.php');
+$query = 'INSERT INTO category_guitar1(categoryName)
+	 VALUES
+	 (:categoryName)';
+	 $statement = $db->prepare($query);
+	 $statement->bindValue(':categoryName', $categoryName;
+	 $statement->execute();
+	 $statement->closeCursor();
+
+include('index.php');
 ?>
