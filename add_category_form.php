a1dacda019dd20085927cef0595f5111fbeebc4e
balakumarhs <bs443@njit.edu>
2017-02-23 22:54:01 -0500

    adding category form php

---
+++ add_category_form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Guitar Shop</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<header><h1>Category Manager</h1></header>
+
+<main>
+	<h1>Add Category</h1>
+</main>
