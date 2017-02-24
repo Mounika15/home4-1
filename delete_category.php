fb4baf565e48620f99a7212e078cbdfd10b47c0f
balakumarhs <bs443@njit.edu>
2017-02-24 00:28:50 -0500

    add delete_category.php

---
+++ delete_category.php
@@ -0,0 +1,16 @@
+<?php
+require_once('database.php');
+
+$categoryID = filter_input(INPUT_POST, 'categoryID', FILTER_VALIDATE_INT);
+if ($category_id != false)
+{
+$query = 'DELETE FROM categories_guitar1
+		where categoryID = :categoryID;
+$statement = $db->prepare($query);
+$statement->bindValue(':categoryID', $categoryID);
+$success = $statement->execute();
+$statement->closeCursor();
+}
+
+include('index.php');
+
