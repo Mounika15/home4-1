78d94dcbbe11825bebf67dad77e197fb04ac2acc
balakumarhs <bs443@njit.edu>
2017-02-23 23:02:31 -0500

    adding rest of the code

--- add_category_form.php
+++ add_category_form.php
@@ -9,4 +9,14 @@
 
 <main>
 	<h1>Add Category</h1>
+	<form action="add_category.php" method="post"id="add_category_form">
+	<label>Category:</label>
+	<input type="text" name="code"><br>
+	<label>&nbsp;</label><br>
+	<input type="submit" value="Add Category"><br>
+	</form>
+	<p><a href="index.php">View Product List</a></p>
+
 </main>
+</body>
+</html>
